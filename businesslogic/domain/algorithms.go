643be9ede83b2b80dfcd7929b8746458ddfc8b51
Lucas Furlani rosa <lucas.furlani.rosa@gmail.com>
2019-06-19 20:37:20 -0300

    Created 'contains' method that identifies if a string contains another and created tests to it

---
+++ businesslogic/domain/algorithms.go
@@ -0,0 +1,20 @@
+package domain
+
+import (
+	"golang.org/x/text/language"
+	"golang.org/x/text/search"
+)
+
+func contains(haystack, needle string) bool {
+	t := language.Tag(language.BrazilianPortuguese)
+	// IgnoreDiacritics is used to match things like á == a
+	matcher := search.New(t, search.IgnoreDiacritics)
+	
+	start, _ := matcher.IndexString(haystack, needle)
+
+	if start == -1 {
+		return false
+	}
+	
+	return true
+}
